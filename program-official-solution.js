9991a197b15f31f44fb9eb38e73e1104640c74ee
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-11-02 10:04:16 +0100

    HTTP CLIENT (Exercise 7 of 13)

--- program-official-solution.js
+++ program-official-solution.js
@@ -1,16 +1,7 @@
-var fs = require('fs')
-var path = require('path')
+var http = require('http')
 
-module.exports = function (dir, filterStr, callback) {
- fs.readdir(dir, function (err, list) {
-   if (err) {
-     return callback(err)
-   }
-
-   list = list.filter(function (file) {
-     return path.extname(file) === '.' + filterStr
-   })
-
-   callback(null, list)
- })
-}
+http.get(process.argv[2], function (response) {
+ response.setEncoding('utf8')
+ response.on('data', console.log)
+ response.on('error', console.error)
+}).on('error', console.error)
