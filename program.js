c533b2a8432969f5ee9c1e5913f6991d20c0dcf4
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-11-02 09:24:57 +0100

    BABY STEPS (Exercise 2 of 13)

--- program.js
+++ program.js
@@ -1,1 +1,11 @@
-console.log("HELLO WORLD")
+// https://nodejs.org/docs/latest/api/process.html#process_process_argv
+
+var sum=0
+
+process.argv.forEach( function(item,i) {
+	if (i>=2) {
+		sum += parseInt(item,10);
+	}
+})
+
+console.log(sum);
