42c4f222b9c81871da045fdd6564630c0500f306
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-11-02 09:31:55 +0100

    MY FIRST ASYNC I/O! (Exercise 4 of 13)

--- program.js
+++ program.js
@@ -1,16 +1,17 @@
 /*
+MY FIRST ASYNC I/O! (Exercise 4 of 13)
 
-MY FIRST I/O! (Exercise 3 of 13)
+Write a program that uses a single asynchronous filesystem operation to read a file and print the number of newlines it contains to the console (stdout), similar to running cat file | wc -l.
 
-Write a program that uses a single synchronous filesystem operation to read a file and print the number of newlines (\n) it contains to the console (stdout), similar to running cat file | wc -l.
-
-The full path to the file to read will be provided as the first command-line argument (i.e., process.argv[2]). You do not need to make your own test file.
+The full path to the file to read will be provided as the first
+command-line argument.
 */
 
 var fs = require('fs')
 var nameFile = process.argv[2];
 
-var contentFile = fs.readFileSync(nameFile).toString()
-var numNewlines = contentFile.split('\n').length - 1;
-
-console.log (numNewlines)
+fs.readFile(nameFile, function(err, contentBuffer) {
+	var contentFile = contentBuffer.toString();
+	var numNewlines = contentFile.split('\n').length - 1;
+	console.log (numNewlines)
+})
