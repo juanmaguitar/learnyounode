d17a86e4b67529bbc5640d73a8eef21bb2f3c90b
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-11-02 18:36:39 +0100

    HTTP JSON API SERVER (Exercise 13 of 13)

--- program-official-solution.js
+++ program-official-solution.js
@@ -1,14 +1,35 @@
 var http = require('http')
-var map = require('through2-map')
+var url = require('url')
+
+function parsetime (time) {
+ return {
+   hour: time.getHours(),
+   minute: time.getMinutes(),
+   second: time.getSeconds()
+ }
+}
+
+function unixtime (time) {
+ return { unixtime: time.getTime() }
+}
 
 var server = http.createServer(function (req, res) {
- if (req.method !== 'POST') {
-   return res.end('send me a POST\n')
+ var parsedUrl = url.parse(req.url, true)
+ var time = new Date(parsedUrl.query.iso)
+ var result
+
+ if (/^\/api\/parsetime/.test(req.url)) {
+   result = parsetime(time)
+ } else if (/^\/api\/unixtime/.test(req.url)) {
+   result = unixtime(time)
  }
 
- req.pipe(map(function (chunk) {
-   return chunk.toString().toUpperCase()
- })).pipe(res)
+ if (result) {
+   res.writeHead(200, { 'Content-Type': 'application/json' })
+   res.end(JSON.stringify(result))
+ } else {
+   res.writeHead(404)
+   res.end()
+ }
 })
-
 server.listen(Number(process.argv[2]))
--- program.js
+++ program.js
@@ -1,29 +1,66 @@
 /*
-HTTP UPPERCASERER (Exercise 12 of 13)
+HTTP JSON API SERVER (Exercise 13 of 13)
 
-Write an HTTP server that receives only POST requests and converts
-incoming POST body characters to upper-case and returns it to the client.
+Write an HTTP server that serves JSON data when it receives a GET request to the path '/api/parsetime'. Expect the request to contain a query string with a key 'iso' and an ISO-format time as the value.
 
-Your server should listen on the port provided by the first argument to
-your program.
+For example:
+
+/api/parsetime?iso=2013-08-10T12:10:15.474Z
+
+The JSON response should contain only 'hour', 'minute' and 'second'
+properties. For example:
+
+ {
+   "hour": 14,
+   "minute": 23,
+   "second": 15
+ }
+
+Add second endpoint for the path '/api/unixtime' which accepts the same query string but returns UNIX epoch time in milliseconds (the number of milliseconds since 1 Jan 1970 00:00:00 UTC) under the  property 'unixtime'.
+
+For example:
+
+ { "unixtime": 1376136615474 }
+
+Your server should listen on the port provided by the first argument to your program.
 
 */
 
 var http = require('http');
-var fs = require('fs');
+var url = require('url');
 var port = process.argv[2];
-
-var map = require('through2-map')
 
 var server = http.createServer()
 
 server.on('request', function (req, res) {
 
-  if (req.method === "POST") {
+	var urlParsed = url.parse(req.url, true);
+	var isoDate = urlParsed.query.iso;
+	var date = new Date(isoDate);
 
-    req.pipe( map(function (chunk) {
-     return chunk.toString().toUpperCase()
-    }) ).pipe(res)
+	var oParsedTime, oUnixTime;
+
+	res.writeHead(200, { 'Content-Type': 'application/json' })
+
+  if (req.method === 'GET') {
+
+  	if (urlParsed.pathname === '/api/parsetime') {
+
+			oParsedTime = {
+       "hour": date.getHours(),
+       "minute": date.getMinutes(),
+       "second": date.getSeconds(),
+     	}
+			res.end( JSON.stringify(oParsedTime) )
+
+  	}
+
+  	if (urlParsed.pathname === '/api/unixtime') {
+  		oUnixTime = {
+       "unixtime": date.getTime()
+     	}
+  		res.end( JSON.stringify(oUnixTime) )
+  	}
 
   }
 
