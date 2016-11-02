cdfed389e9c68e4f0eaf8c212ab224e1cf647bff
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-11-02 16:23:54 +0100

    HTTP UPPERCASERER (Exercise 12 of 13)

--- program-official-solution.js
+++ program-official-solution.js
@@ -1,10 +1,14 @@
 var http = require('http')
-var fs = require('fs')
+var map = require('through2-map')
 
 var server = http.createServer(function (req, res) {
- res.writeHead(200, { 'content-type': 'text/plain' })
+ if (req.method !== 'POST') {
+   return res.end('send me a POST\n')
+ }
 
- fs.createReadStream(process.argv[3]).pipe(res)
+ req.pipe(map(function (chunk) {
+   return chunk.toString().toUpperCase()
+ })).pipe(res)
 })
 
 server.listen(Number(process.argv[2]))
--- program.js
+++ program.js
@@ -1,25 +1,32 @@
 /*
-HTTP FILE SERVER (Exercise 11 of 13)
+HTTP UPPERCASERER (Exercise 12 of 13)
 
-Write an HTTP server that serves the same text file for each request it receives.
+Write an HTTP server that receives only POST requests and converts
+incoming POST body characters to upper-case and returns it to the client.
 
 Your server should listen on the port provided by the first argument to
 your program.
-
-You will be provided with the location of the file to serve as the second command-line argument. You must use the fs.createReadStream() method to stream the file contents to the response.
 
 */
 
 var http = require('http');
 var fs = require('fs');
 var port = process.argv[2];
-var fileToRead = process.argv[3];
+
+var map = require('through2-map')
 
 var server = http.createServer()
 
 server.on('request', function (req, res) {
-  fs.createReadStream(fileToRead)
-    .pipe(res)
+
+  if (req.method === "POST") {
+
+    req.pipe( map(function (chunk) {
+     return chunk.toString().toUpperCase()
+    }) ).pipe(res)
+
+  }
+
 })
 
 server.listen(port)
